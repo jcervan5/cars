8d1e584202d58826bd3d52e7c222b9c6300ea43e
Jasmine Cervantes <jt24cervantes@yahoo.com>
2017-07-27 15:35:31 -0600

    almost

---
+++ README.md
@@ -0,0 +1,5 @@
+ERD-https://www.lucidchart.com/documents/edit/f185292d-c601-4134-8b8c-a7c0d2d8b194
+
+heroku - https://safe-beach-17148.herokuapp.com/camaro
+
+firebase - https://camaro-7fe5e.firebaseapp.com/
--- client/app.js
+++ client/app.js
@@ -2,12 +2,4 @@ $(document).ready(function(){
 
 
 })
-const url = "http://localhost:5000/camaro"
-const form = document.querySelector('form')
-
-form.addEventListener("findCar", sendPostRequest)
-
-function sendPostRequest(event) {
-  event.preventDefault
-  console.log("hello");
-}
+const url = "http://localhost:3000/camaro"
